b9818669c53f336960a9815761bcc503439a7820
Zixuan Pan <pan.zix@northeastern.edu>
2025-06-01 08:13:46 -0400

    configuring controllers

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,32 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @ClassName BookController
+ * @Description TODO
+ * @Author Panzi
+ * @Date 2025/5/31 10:06
+ * @Version 1.0
+ */
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+
+}
